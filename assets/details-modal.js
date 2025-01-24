da77cc1bfd67f2680b4b0ecc53541d8f0b3a2904
Larry Ioannidis <larry@rakun.ie>
2025-01-24 15:05:09 +0000

    remove modal scroll stop

--- assets/details-modal.js
+++ assets/details-modal.js
@@ -28,7 +28,7 @@ class DetailsModal extends HTMLElement {
     this.onBodyClickEvent = this.onBodyClickEvent || this.onBodyClick.bind(this);
     event.target.closest('details').setAttribute('open', true);
     document.body.addEventListener('click', this.onBodyClickEvent);
-    document.body.classList.add('overflow-hidden');
+    // document.body.classList.add('overflow-hidden');
 
     trapFocus(
       this.detailsContainer.querySelector('[tabindex="-1"]'),
@@ -40,7 +40,7 @@ class DetailsModal extends HTMLElement {
     removeTrapFocus(focusToggle ? this.summaryToggle : null);
     this.detailsContainer.removeAttribute('open');
     document.body.removeEventListener('click', this.onBodyClickEvent);
-    document.body.classList.remove('overflow-hidden');
+    // document.body.classList.remove('overflow-hidden');
   }
 }
 
